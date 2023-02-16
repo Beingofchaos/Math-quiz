8fac47061b6a87cef6ebd7b0f3e57c7d76dacac1
Beingofchaos <123133234+Beingofchaos@users.noreply.github.com>
2023-02-15 20:45:38 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -19,9 +19,9 @@ document.getElementById("player_answer").innerHTML = "Answer Turn " + player2_na
 function send() {
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
-    actual_answer = pareInt(number)* parseInt(number2);
+    actual_answer = parseInt(number1)* parseInt(number2);
     console.log(actual_answer);
-    question_number = "<h4>" + "X"+ number2 +"</h4>";
+    question_number = "<h4>" + number1 +  " X " + number2 +"</h4>";
     input_box = "<br>Answer : <input type='text' id= 'input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick= 'check()'>Check</button>";
     row = question_number + input_box + check_button;
@@ -41,18 +41,18 @@ answer_turn = "player2";
 function check()
 {
     get_answer = document.getElementById("input_check_box").value;
-    answer = get_answer.toLowerCase();
-    console.log("answer in lower case -"+ answer);
-    if(answer == word)
-    { if(answer_turn == "player1")
-    { player1_score = player1_score +1;
+  if(answer_turn == actual_answer)
+  {
+    
+     if(answer_turn == "player1")
+    {player1_score = player1_score +1;
     document.getElementById("player1_score").innerHTML = player1_score;
 }
 else{
      player2_score = player2_score +1;
         document.getElementById("player2_score").innerHTML = player2_score;
 
-
+}
 }
     if(question_turn == "player1")
     {
@@ -73,4 +73,4 @@ else{
     document.getElementById("player_answer").innerHTML = "Answer Turn -" + player1_name;
 }
 document.getElementById("output").innerHTML = "";
-} }
+} 
